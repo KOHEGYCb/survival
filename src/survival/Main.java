ba7eea81ca72f88dc91313736557913b580ea096
KOHEGYCb <dimaushenko@mail.ru>
2017-01-02 21:39:42 +0300

    create NB project

---
+++ src/survival/Main.java
@@ -0,0 +1,16 @@
+package survival;
+
+/**
+ *
+ * @author Dmitry
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
