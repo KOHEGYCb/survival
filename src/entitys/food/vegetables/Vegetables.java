ae279091cd4c5b97c974daa4bb75f7fc40bb0257
Adromar <t2lt2014@mail.ru>
2017-01-11 21:07:30 +0300

    3

--- src/entitys/food/vegetables/Vegetables.java
+++ src/entitys/food/vegetables/Vegetables.java
@@ -1,14 +1,14 @@
 package entitys.food.vegetables;
 
 import constants.VegetablesEnum;
-import entitys.food.Food;
+        import entitys.food.Food;
 
 /**
  *
  * @author Dmitry
  */
-public abstract class Vegetables extends Food{  
-    
+public abstract class Vegetables extends Food{
+
     public Vegetables(int weight, int volume, VegetablesEnum vegetablesEnum) {
         super(vegetablesEnum.getKkal(), weight, volume, vegetablesEnum.getName(), vegetablesEnum.getDescription());
     }
